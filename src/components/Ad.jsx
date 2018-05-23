333d1b01a6ab08cc133a6e96a8bc0c4c48762096
handsomehan <handsomehan515@gmail.com>
2018-05-23 17:02:24 +0800

    使用 react PureComponent

--- src/components/Ad.jsx
+++ src/components/Ad.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-class Ad extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    // console.log('this props: %o, next props: %o, next state: %o', this.props, nextProps, nextState)
-    if (this.props.name === nextProps.name) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+class Ad extends PureComponent {
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   // console.log('this props: %o, next props: %o, next state: %o', this.props, nextProps, nextState)
+  //   if (this.props.name === nextProps.name) {
+  //     return false;
+  //   } else {
+  //     return true;
+  //   }
+  // }
 
   render() {
     console.log(this.props)
