6ea8140163adad2ba07aab97664d13fea9108448
handsomehan <handsomehan515@gmail.com>
2018-06-04 19:07:14 +0800

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -36,3 +36,20 @@ export const updateList = payload => ({
   type: UPDATE_LIST,
   payload
 })
+
+const INCREMENT_COUNTER = 'INCREMENT_COUNTER';
+
+function increment() {
+  return {
+    type: INCREMENT_COUNTER
+  };
+}
+
+function incrementAsync() {
+  return dispatch => {
+    setTimeout(() => {
+      // Yay! Can invoke sync or async actions with `dispatch`
+      dispatch(increment());
+    }, 1000);
+  };
+}
