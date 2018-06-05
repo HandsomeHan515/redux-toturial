b565fa9f7ec1ad5553f747c207808bd69998d98a
handsomehan <handsomehan515@gmail.com>
2018-06-05 09:05:25 +0800

    update

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,11 +2,17 @@ import { call, put, takeEvery, takeLatest, take } from 'redux-saga/effects'
 import * as actionTypes from '../actions';
 import * as api from '../api';
 
+const delay = function (time) {
+  setTimeout(function () {
+    console.log(123);
+    return 123;
+  }, time);
+}
+
 function* mySaga() {
   const han = yield take(actionTypes.FETCH_FLOWERS);
-  console.log(han)
   try {
-    const flowers = yield call(api.getFlowers);
+    const flowers = yield call(delay, 1000);
     console.log('flowers: %o', flowers);
     yield put({ type: 'FETCH_FLOWERS_SUCCESS', flowers });
   } catch (e) {
